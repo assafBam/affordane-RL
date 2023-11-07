c3a694ae83014868e31d976ef90b4b26e3dc26d1
assaf Bamberger <50671687+assafBam@users.noreply.github.com>
2023-11-07 19:35:01 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -24,14 +24,7 @@ Observation = namedtuple('Observation', ['robot_x', 'robot_y', 'robot_theta', 'b
 
 
 def make_step(action: tuple[float, float])-> dict[str, float]:
-    pass # returns the location of the ball and the location of the robot
-    # MU = 0.4
-    # g = 9.8
-    # v_ball = np.sqrt(MASS_ROBOT/MASS_BALL)*action[0]
-    # ell = v_ball * TIME_BOUND # x=vt
-    # roll_theta = action[1]-np.pi # the ball rolling vector is 180 degrees diferrent from the angle of which the robot impact it
-    # x,y = ell*np.cos(roll_theta), ell*np.sin(roll_theta)
-    # return {'robot_x':BALL_INIT_POS[0],'robot_y':BALL_INIT_POS[1],'robot_theta':action[1],'ball_x':x,'ball_y':y}
+    # returns the location of the ball and the location of the robot
     ball_pos = run_commands(action[0], action[1], COMMANDS_FILE)
     return {'robot_x':BALL_INIT_POS[0],'robot_y':BALL_INIT_POS[1],'robot_theta':action[1],'ball_x':ball_pos[0],'ball_y':ball_pos[1]}
 
@@ -39,10 +32,6 @@ def distance(p1, p2):
     return np.sqrt(np.power(p1[0]-p2[0],2)+np.power(p1[1]-p2[1], 2))
 
 def calculate_reward(ball_loc, target): #TODO: save all rewards and print them to a nice graph
-    # EPSILON = 10
-    # tmp = np.sqrt(np.power(ball_loc[0]-target[0],2)+np.power(ball_loc[1]-target[1], 2))
-    # rewards.append(EPSILON - tmp**3)
-    # return EPSILON - tmp**3
     GAMMA = 10
     res = -np.log(GAMMA*distance(ball_loc, target))
     return res
@@ -53,10 +42,8 @@ class AffordanceEnv(gym.Env):
     def __init__(self, *args, log_file=None, **kwargs):
         super().__init__()
         # other things
-        #   action: V,theta
-        #TODO: change to lower speed boundries?
+        #   action: V, theta
         self.action_space = spaces.Box(low=np.array([0,0]), high=np.array([MAX_SPEED, 2* np.pi]), dtype=np.float32) # angle in radians
-        # self.action_space = spaces.Box(low=np.array([0,0]), high=np.array([100, 0.1]), dtype=np.float32)
         #   observation: robot_x, robot_y, robot_theta, ball_x, ball_y, target_x, target_y
         self.observation_space = spaces.Box(low=np.array([-inf,-inf, 0, -inf, -inf, -inf,-inf]),high=np.array([inf, inf, 360, inf, inf, inf, inf]), dtype=np.float32)
         self.target = None
